bed0deabb0184a99d0dc3aa24257d59fba7bd7ea
abhishek17551 <ababhishek113@gmail.com>
2024-02-01 22:24:38 +0530

    chores:installed,configured tailwind

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="text-gray-500 font-bold">
         Start coding the Netflix-GPT project!!!
     </div>
   );
