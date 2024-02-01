be924f03f5dfb5c84e9c4e5f29601df952a9495b
abhishek17551 <ababhishek113@gmail.com>
2024-02-01 22:36:03 +0530

    chores:installed and configured basic routing

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import Login from './Login'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
 const Body = () => {
+
+    const appRouter = createBrowserRouter([
+        {
+            path : "/",
+            element : <Login/>
+        }
+    ])
   return (
     <div>
-        <Login/>
+        <RouterProvider router={appRouter}/>
+        
     </div>
   )
 }
