7a290fcb30fd41e0140d41333e785bbd104499f4
abhishek17551 <ababhishek113@gmail.com>
2024-02-02 19:56:51 +0530

    feat:implemented firebase login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,14 @@
 import React, { useRef, useState } from 'react'
 import { checkValidation } from '../utils/validate';
 import { createUserWithEmailAndPassword } from "firebase/auth";
+import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from '../utils/firebase';
 
 const Login = () => {
     const [isSignInForm,setIsSignInForm] = useState(true);
     const [errorMessage,setErrorMessage] = useState(null)
 
-    const username = useRef(null)
+    //const username = useRef(null)
     const email = useRef(null)
     const password = useRef(null)
 
@@ -16,13 +17,17 @@ const Login = () => {
     }
 
     const validateForm = () => {
-        const message = checkValidation(username.current.value,email.current.value,password.current.value)
+        if ( !email.current || !password.current) {
+            console.error("One or more refs are not initialized.");
+            return;
+        }
+        const message = checkValidation( email.current.value, password.current.value)
         setErrorMessage(message)
 
         if(message) return;
 
         if(!isSignInForm) {
-            createUserWithEmailAndPassword(auth, email.current.value,password.current.value)
+            createUserWithEmailAndPassword(auth, email.current.value, password.current.value)
                 .then((userCredential) => {
                   // Signed up 
                   const user = userCredential.user;
@@ -35,16 +40,26 @@ const Login = () => {
                 });
         }
         else {
-
+            signInWithEmailAndPassword(auth,email.current.value,password.current.value)
+                .then((userCredential) => {
+                    const user = userCredential.user;
+                    console.log(user)
+                })
+                .catch((error) => {
+                    const errorCode = error.code;
+                    const errorMessage = error.message;
+                    setErrorMessage(errorCode + " - " + errorMessage)
+                })
         }
 
     }
+
   return (
     
         <form onSubmit={(e) => e.preventDefault()} className='rounded-lg absolute bg-black flex flex-col my-52 mx-auto right-0 left-0 w-3/12 text-white p-4 bg-opacity-80'>
             <h1 className='font-bold mb-1 text-2xl'>{isSignInForm ? "Sign In" : "Sign Up"}</h1>
             {
-                !isSignInForm && <input ref={username} type='text' placeholder='Enter Full Name' className='p-3 m-3 bg-slate-600 cursor-text'/>
+                !isSignInForm && <input type='text' placeholder='Enter Full Name' className='p-3 m-3 bg-slate-600 cursor-text'  />
             }
             <input ref={email} type='text' placeholder='Enter Email Address' className='p-3 m-3 bg-slate-600 cursor-text rounded-md'/>
             <input ref={password} type='password' placeholder='Enter Password' className='p-3 m-3 bg-slate-600 rounded-md' />
--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,9 +1,8 @@
-export const checkValidation = (username,email,password) => {
-    const isUsernameValid = /^[A-Za-z][A-Za-z0-9_]{7,29}$/.test(username)
+export const checkValidation = (email,password) => {
     const isEmailValid = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/.test(email)
     const isPasswordValid = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password)
      
-    if(!isUsernameValid) return 'Invalid Username'
+   
     if(!isEmailValid) return "Invalid Email ID"
     if(!isPasswordValid) return "Invalid Password"
 
