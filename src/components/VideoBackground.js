091bf2ef9a972d8f36b5a1d5705ae3914cd51313
abhishek17551 <ababhishek113@gmail.com>
2024-02-06 01:06:48 +0530

    refactor:extracted fetching movie trailer logic to useMovieTrailer custom hook

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -1,22 +1,12 @@
 import React, { useEffect } from 'react'
-import { API_OPTIONS } from '../utils/constants'
-import { useDispatch, useSelector } from 'react-redux'
-import { addTrailerVideo } from '../utils/slices/moviesSlice'
+import { useSelector } from 'react-redux'
+import useMovieTrailer from '../hooks/useMovieTrailer'
+
 
 const VideoBackground = ({movieId}) => {
-    const dispatch = useDispatch();
     const trailerVideo = useSelector((store) => store.movies?.trailerVideo)
 
-    const getVideo = async () => {
-        const data = await fetch(`https://api.themoviedb.org/3/movie/${movieId}/videos`,API_OPTIONS)
-        const json = await data.json()
-        const trailer = json.results.find((video) => video.type === "Trailer")
-        dispatch(addTrailerVideo(trailer))
-    }
-
-    useEffect(() => {
-        getVideo()
-    },[])
+    useMovieTrailer(movieId)
   return (
     <div>
         <iframe className='w-full h-[35rem] absolute' src={"https://www.youtube.com/embed/" + trailerVideo?.key} title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" ></iframe>
---
+++ src/hooks/useMovieTrailer.js
@@ -0,0 +1,21 @@
+import { useDispatch, useSelector } from "react-redux";
+import { API_OPTIONS } from "../utils/constants";
+import { addTrailerVideo } from "../utils/slices/moviesSlice";
+import { useEffect } from "react";
+
+const useMovieTrailer = (movieId) => {
+    const dispatch = useDispatch();
+
+    const getVideo = async () => {
+        const data = await fetch('https://api.themoviedb.org/3/movie/'+ movieId +'/videos',API_OPTIONS)
+        const json = await data.json()
+        const trailer = json.results.find((video) => video.type === "Trailer")
+        dispatch(addTrailerVideo(trailer))
+    }
+
+    useEffect(() => {
+        getVideo()
+    },[])
+}
+
+export default useMovieTrailer
