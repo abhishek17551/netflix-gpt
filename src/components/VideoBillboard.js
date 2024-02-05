0b6bdd60c293f5401be00e5710678d08dd757139
abhishek17551 <ababhishek113@gmail.com>
2024-02-05 23:41:37 +0530

    created basic component skeleton for Home page

---
+++ src/components/VideoBillboard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const VideoBillboard = () => {
+  return (
+    <div>VideoBillboard</div>
+  )
+}
+
+export default VideoBillboard
---
+++ src/components/VideoRows.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const VideoRows = () => {
+  return (
+    <div>VideoRows</div>
+  )
+}
+
+export default VideoRows
