65324191a4ed317408082feca151b3015e7af35d
abhishek17551 <ababhishek113@gmail.com>
2024-02-05 23:24:19 +0530

    fetched now playing movies list, created moviesSlice & provided to store

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Header from './Header'
+import { API_OPTIONS, NOW_PLAYING_API } from '../utils/constants'
+import { useDispatch } from 'react-redux'
+import { addNowPlayingMovies } from '../utils/slices/moviesSlice'
 
 const Home = () => {
+
+    const dispatch = useDispatch();
+
+    const getNowPlayingMovies = async () => {
+        const data = await fetch(NOW_PLAYING_API,API_OPTIONS)
+        const json = await data.json();
+        //console.log(json.results)
+        dispatch(addNowPlayingMovies(json.results))
+    }
+
+    useEffect(() => {
+        getNowPlayingMovies();
+    },[])
+
   return (
     <div>
         <Header/>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,3 +3,14 @@ export const BRAND_LOGO = "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9c
 export const LANDING_PAGE_BG = "https://assets.nflxext.com/ffe/siteui/vlv3/9134db96-10d6-4a64-a619-a21da22f8999/a449fabb-05e4-4c8a-b062-b0bec7d03085/IN-en-20240115-trifectadaily-perspective_alpha_website_large.jpg" 
 
 export const USER_LOGO = "https://occ-0-4994-2164.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABTZ2zlLdBVC05fsd2YQAR43J6vB1NAUBOOrxt7oaFATxMhtdzlNZ846H3D8TZzooe2-FT853YVYs8p001KVFYopWi4D4NXM.png?r=229"
+
+export const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZmFjYTllMjZkNDNjYzk0NTNlODM2ZjBmYmY0MjMwMCIsInN1YiI6IjY1YzBlZWM2OTAyMDEyMDE0OGNhYTliNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0oHa1Dh8r8XHvRGskaN9PU2q8Ld9q-sspqIzAimSIvU'
+    }
+  };
+
+export const NOW_PLAYING_API = 'https://api.themoviedb.org/3/movie/now_playing?page=1';
+  
