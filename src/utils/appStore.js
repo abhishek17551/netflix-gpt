64a6ba196ef508b97f003b9f85256001c5b3f41b
abhishek17551 <ababhishek113@gmail.com>
2024-02-06 23:11:58 +0530

    declared lang constants,created lang slice & provided to store

--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import  userReducer  from "../utils/slices/userSlice"
 import moviesReducer from "./slices/moviesSlice";
 import gptReducer from './slices/gptSlice'
+import languageReducer from './slices/languageSlice'
 
 const appStore = configureStore({
     reducer : {
         user : userReducer,
         movies : moviesReducer,
-        gpt : gptReducer
+        gpt : gptReducer,
+        language : languageReducer
     }
 })
 
---
+++ src/utils/languageConstants.js
@@ -0,0 +1,35 @@
+export const lang = {
+    en: {
+      greeting : "Hello",
+      search: "Search",
+      gptSearchPlaceholder: 'Hit me up with what you want watch today...',
+    },
+    kannada: {
+      greeting : "",
+      search: "ನಮಸ್ಕಾರ",
+      gptSearchPlaceholder: "ಇಂದು ನೀವು ಏನನ್ನು ವೀಕ್ಷಿಸಲು ಬಯಸುತ್ತೀರೋ ಅದನ್ನು ನನಗೆ ಹೇಳು...",
+    },
+    german: {
+      greeting : 'Hallo',
+      search: "Suchen",
+      gptSearchPlaceholder: "Schreiben Sie mir, was Sie heute sehen möchten ...",
+    },
+  };
+
+export const  SUPPORTED_LANGUAGES = [
+    {
+        identifier : 'en',
+        name : 'English'
+    },
+
+    {
+        identifier : 'kannada',
+        name : 'Kannada'
+    },
+
+    {
+        identifier : 'german',
+        name : 'German'
+    },
+]
+  
